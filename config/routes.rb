a30c437f075ca4417b8a9590162ca7ae9e79ea87
Sonia Bisinger <sonia.bisinger@gmail.com>
2023-05-23 16:26:22 +0200

    2nd exercices, program working, missing refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #show all tasks
+  get "tasks", to: "tasks#index"
 
+  #create a new task 
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+
+  # show one task
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  # edit a task 
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+
+  #delete
+  delete "tasks/:id", to: "tasks#destroy"
+ 
   # Defines the root path route ("/")
   # root "articles#index"
 end
